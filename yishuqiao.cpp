ee18fdf6a7990e60c75a0941b32e75bb95ed63f0
Marikruz <783165069@qq.com>
2017-10-01 12:15:14 +0800

    hello-world

---
+++ yishuqiao.cpp
@@ -0,0 +1,48 @@
+#include<stdio.h>
+void A(void);
+void B(void);
+void C(void);
+void D(void);
+void E(void);
+void F(void);
+int main(void)
+{
+	printf("易书桥具有以下功能：\n");
+	A();
+	B();
+	C();
+	D();
+	E();
+	F();
+  return 0; 
+}
+
+void A(void)
+ {
+  printf("多彩社团筛选；\n");
+ }
+
+void B(void)
+ {
+  printf("书籍书房管理；\n");
+ }
+
+void C(void)
+ {
+  printf("个性课表管理；\n");
+  } 
+
+void D(void)
+ {
+  printf("二手教材买卖；\n");
+ }
+
+void E(void)
+ {
+  printf("专业试题锦集；\n");
+ } 
+ 
+void F(void)
+ {
+  printf("组团低价求书.\n");
+ } 
