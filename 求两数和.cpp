c2c320d6a7e70c2a4dbfe196872614b201428bfd
Marikruz <783165069@qq.com>
2017-10-02 20:05:35 +0800

    求两数和

---
+++ 求两数和.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ int main() 
+ {int a,b,c,d,e;
+   printf("请输入两个数\n");
+   scanf("%d %d",&a,&b);
+   
+   c=a+b;
+   printf("两数之和为%d\n",c);
+   
+   d=a-b;
+   printf("两数之差为%d\n",d);  
+   
+   e=a*b;
+   printf("两数之积为%d\n",e);
+ }
