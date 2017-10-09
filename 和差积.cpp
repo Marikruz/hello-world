aa1092b1aac63c37bf2982b194bf92d43e498be9
Marikruz <783165069@qq.com>
2017-10-09 22:47:53 +0800

    和差积

---
+++ 和差积.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() 
+ {int a,b;
+   printf("请输入两个数\n");
+   scanf("%d %d",&a,&b);
+
+   printf("两数之和为%d\n",a+b);
+
+   printf("两数之差为%d\n",a-b);  
+
+   printf("两数之积为%d\n",a*b);
+ }
